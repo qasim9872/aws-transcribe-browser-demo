073ec34dc294dd1419472ab5fe64b981c0ee8e7b
Muhammad Qasim <qasim9872@gmail.com>
2020-04-16 17:17:10 +0100

    added recording logic

--- src/js/components/helper.js
+++ src/js/components/helper.js
@@ -6,9 +6,8 @@ export function addInputElements(inputFieldDiv, inputs) {
     labelElement.textContent = name;
     inputFieldDiv.appendChild(labelElement);
 
-    const inputElement = document.createElement('input', {
-      id,
-    });
+    const inputElement = document.createElement('input');
+    inputElement.id = id;
     inputElement.placeholder = `${name} Here`;
     inputElement.type = type;
     inputFieldDiv.appendChild(inputElement);
@@ -25,4 +24,51 @@ export function createDisplayElements(displayDiv, name, inputs) {
   addInputElements(inputFieldDiv, inputs);
 
   displayDiv.appendChild(inputFieldDiv);
+}
+
+export function downsampleBuffer(buffer, sampleRate, outSampleRate) {
+  if (outSampleRate === sampleRate) {
+    return buffer;
+  }
+  if (outSampleRate > sampleRate) {
+    // throw error
+    console.log('down sampling rate show be smaller than original sample rate');
+  }
+  const sampleRateRatio = sampleRate / outSampleRate;
+  const result = new Int16Array(Math.round(buffer.length / sampleRateRatio));
+  let offsetResult = 0;
+  let offsetBuffer = 0;
+  while (offsetResult < result.length) {
+    const nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+    let accum = 0;
+    let count = 0;
+    for (let i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+      accum += buffer[i];
+      count++;
+    }
+    result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
+    offsetResult++;
+    offsetBuffer = nextOffsetBuffer;
+  }
+  return result.buffer;
+}
+
+export function getAwsConfig() {
+  const accessKeyElem = document.getElementById('accessKey');
+  const secretAccessKeyElem = document.getElementById('secretAccessKey');
+
+  return {
+    accessKeyId: accessKeyElem.value,
+    secretAccessKey: secretAccessKeyElem.value,
+  };
+}
+
+export function getTranscribeConfig() {
+  const languageElem = document.getElementById('language');
+  const regionElem = document.getElementById('region');
+
+  return {
+    language: languageElem.value,
+    region: regionElem.value,
+  };
 }
--- src/js/components/streaming-manager.js
+++ src/js/components/streaming-manager.js
@@ -1,4 +1,40 @@
+import { downsampleBuffer, getAwsConfig, getTranscribeConfig } from './helper';
+
+const bufferSize = 8192;
+const sampleRateHertz = 16000;
+
+const constraints = {
+  audio: true,
+  video: false,
+};
+
+const TheAudioContext =
+  window.AudioContext || // Default
+  window.webkitAudioContext || // Safari and old versions of Chrome
+  false;
+
+// computes the correct navigator
+navigator.mediaDevices.getUserMedia =
+  navigator.mediaDevices.getUserMedia ||
+  navigator.mediaDevices.webkitGetUserMedia ||
+  navigator.mediaDevices.mozGetUserMedia;
+
+if (!navigator.mediaDevices) {
+  console.warn(
+    "Your browser doesn't have the support for media devices.",
+    'Please upgrade or change the browser to access media devices'
+  );
+}
+
 class StreamingManager {
+  constructor() {
+    this.streaming = false;
+    this.context = null;
+    this.processor = null;
+    this.input = null;
+    this.globalStream = null;
+  }
+
   init() {
     this.manager = document.getElementById('streaming-manager');
     this.manager.classList = 'streaming-window';
@@ -7,14 +43,95 @@ class StreamingManager {
     heading.innerHTML = 'Streaming';
     this.manager.appendChild(heading);
 
+    this.setupAudioResources();
     this.setupDisplay();
     this.setupButtons();
   }
 
-  setupDisplay() {
-    this.displayText = document.createElement('textarea', {
-      id: 'display-text',
+  setupAudioResources() {
+    // create a context which will be used by stt and tts
+    this.context = new TheAudioContext({
+      latencyHint: 'interactive',
     });
+  }
+
+  setupAudioProcessor() {
+    // setup the audio processor
+    this.processor = this.context.createScriptProcessor(bufferSize, 1, 1);
+
+    this.processor.onaudioprocess = (e) => {
+      const buffer = e.inputBuffer.getChannelData(0);
+
+      const pcmBuffer = downsampleBuffer(buffer, 44100, sampleRateHertz);
+
+      this.sendAudioBuffer(pcmBuffer);
+    };
+
+    // connect the processor to the context
+    this.processor.connect(this.context.destination);
+  }
+
+  sendAudioBuffer(buffer) {
+    console.log(`sending audio`);
+  }
+
+  async startStreaming() {
+    console.log('start streaming');
+    this.toggleButtons(true);
+
+    this.awsConfig = getAwsConfig();
+    this.transcribeConfig = getTranscribeConfig();
+
+    // TODO - create transcribe client
+
+    if (!this.processor) {
+      this.setupAudioProcessor();
+    }
+
+    if (this.context.state === 'suspended') {
+      this.context.resume();
+    }
+
+    // create a reference to the stream so we can close it after
+    this.globalStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    // setup an input from the stream and connect it to the processor
+    this.input = this.context.createMediaStreamSource(this.globalStream);
+    this.input.connect(this.processor);
+  }
+
+  stopStreaming() {
+    console.log('stop streaming');
+    this.toggleButtons(false);
+
+    // request to stop recognition
+    this.globalStream.getTracks()[0].stop();
+    this.globalStream = null;
+
+    // input needs to be created from the global stream which may change so we can disconnect the last input
+    this.input.disconnect(this.processor);
+    this.input = null;
+
+    // disconnect the processor so there's nothing happening in h
+    this.processor.disconnect(this.context.destination);
+    this.processor = null;
+  }
+
+  toggleButtons(streaming) {
+    this.streaming = streaming === undefined ? !this.streaming : streaming;
+
+    if (this.streaming) {
+      this.startButton.disabled = true;
+      this.stopButton.disabled = false;
+    } else {
+      this.startButton.disabled = false;
+      this.stopButton.disabled = true;
+    }
+  }
+
+  setupDisplay() {
+    this.displayText = document.createElement('textarea');
+    this.displayText.id = 'display-text';
     this.displayText.classList = 'display-text-window';
     this.displayText.placeholder = 'Your text will show up here';
     this.displayText.rows = 10;
@@ -23,18 +140,19 @@ class StreamingManager {
   }
 
   setupButtons() {
-    this.startButton = document.createElement('button', {
-      id: 'start-streaming',
-    });
+    this.startButton = document.createElement('button');
+    this.startButton.id = 'start-streaming';
     this.startButton.innerText = 'Start';
     this.startButton.classList = 'action-button start';
 
-    this.stopButton = document.createElement('button', {
-      id: 'stop-streaming',
-    });
+    this.startButton.addEventListener('click', this.startStreaming.bind(this));
+
+    this.stopButton = document.createElement('button');
+    this.stopButton.id = 'stop-streaming';
     this.stopButton.innerText = 'Stop';
     this.stopButton.classList = 'action-button stop';
     this.stopButton.disabled = true;
+    this.stopButton.addEventListener('click', this.stopStreaming.bind(this));
 
     const div = document.createElement('div');
     div.appendChild(this.startButton);
